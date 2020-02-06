9fe15e19f24e2fd260946bd2d796d76432e5459b
jeylau <jlauer@kinetikos.io>
2020-02-06 14:40:51 -0500

    Minor cleaning.

--- dlclive/camera/stream.py
+++ dlclive/camera/stream.py
@@ -7,16 +7,18 @@ import tkinter as tk
 import threading
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from queue import LifoQueue
+from time import time
 
 
 class StreamReader(threading.Thread):
     def __init__(self, video_path, max_queue_size=20):
         super(StreamReader, self).__init__(target=self.read, daemon=True)
         self.video = cv2.VideoCapture(video_path)
-        self.video_height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.video_width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.streaming = True
+        self.height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.nframes = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
         self.queue = LifoQueue(maxsize=max_queue_size)
+        self.streaming = True
 
     def read(self):
         while self.streaming:
@@ -30,45 +32,50 @@ class StreamReader(threading.Thread):
     def stop(self):
         self.streaming = False
 
+    def test_speed(self):
+        start = time()
+        success = True
+        while success:
+            success, frame = self.video.read()
+        end = time()
+        print(f'fps={self.nframes / (end - start)}')
+
 
 class StreamDisplay(FigureCanvasTkAgg):
     def __init__(self, stream, parent, dpi=100, **kwargs):
         self.stream = stream
         self.parent = parent
 
-        figsize = self.stream.video_width // dpi, self.stream.video_height // dpi
+        figsize = self.stream.width // dpi, self.stream.height // dpi
         self.fig = plt.Figure(frameon=False, figsize=figsize, dpi=dpi)
         super(StreamDisplay, self).__init__(self.fig, master=parent)
         self.ax = self.fig.add_subplot(111)
-        img = np.random.random((self.stream.video_height, self.stream.video_width, 3))
-        self.im = self.ax.imshow(img)
+        img = np.random.random((self.stream.height, self.stream.width, 3))
+        self.im = self.ax.imshow(img, interpolation='none')
         nbodyparts = 4
         cmap = plt.cm.get_cmap('winter', nbodyparts)
         colors = cmap(range(nbodyparts))
         self.scat = self.ax.scatter([], [], **kwargs)
         self.scat.set_color(colors)
-        self.ax.set_xlim(0, self.stream.video_width)
-        self.ax.set_ylim(0, self.stream.video_height)
+        self.ax.set_xlim(0, self.stream.width)
+        self.ax.set_ylim(0, self.stream.height)
         self.ax.axis('off')
         self.ax.invert_yaxis()
         self.draw()
         self.background = self.copy_from_bbox(self.ax.bbox)
 
-    def refresh(self, ms=20):
+    def refresh(self, ms=10):
         if not self.stream.queue.empty():
             frame = self.stream.queue.get()
             self.im.set_data(frame)
-            points = np.c_[np.random.randint(0, self.stream.video_height, 4),
-                           np.random.randint(0, self.stream.video_width, 4)]
+            points = np.c_[np.random.randint(0, self.stream.height, 4),
+                           np.random.randint(0, self.stream.width, 4)]
             self.scat.set_offsets(points)
             self.restore_region(self.background)
             self.ax.draw_artist(self.im)
             self.ax.draw_artist(self.scat)
             self.blit(self.ax.bbox)
         self.parent.after(ms, self.refresh)
-        # else:
-        #     plt.close(self.fig)
-        #     self.parent.destroy()
 
 
 class StreamWindow(tk.Toplevel):
@@ -85,8 +92,9 @@ class StreamWindow(tk.Toplevel):
         self.start_button.grid(row=1, column=0)
 
 
-video = '/Users/Jessy/Documents/PycharmProjects/dlcdev/examples/Reaching-Mackenzie-2018-08-30/videos/reachingvideo1.avi'
-stream = StreamReader(video)
-root = tk.Tk()
-window = StreamWindow(stream, root, s=12, alpha=0.7)
-root.mainloop()
+if __name__ == '__main__':
+    video = '/Users/Jessy/Documents/PycharmProjects/lactic/videos_hq/davidrudisha.mp4'
+    stream = StreamReader(video)
+    root = tk.Tk()
+    window = StreamWindow(stream, root, s=12, alpha=0.7)
+    root.mainloop()
