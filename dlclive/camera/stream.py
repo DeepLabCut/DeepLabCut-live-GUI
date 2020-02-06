744279d2f9267c71800daca817de458026108ff3
jeylau <jlauer@kinetikos.io>
2020-02-06 16:34:19 -0500

    Rewrite the whole thing in PyQt.

--- dlclive/camera/stream.py
+++ dlclive/camera/stream.py
@@ -1,33 +1,30 @@
-import matplotlib
-matplotlib.use('TkAgg')
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-import tkinter as tk
+import pyqtgraph as pg
+import sys
 import threading
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from queue import LifoQueue
+from collections import deque
+from pyqtgraph.Qt import QtCore, QtWidgets
 from time import time
 
 
 class StreamReader(threading.Thread):
-    def __init__(self, video_path, max_queue_size=20):
+    def __init__(self, video_path, max_queue_size=100):
         super(StreamReader, self).__init__(target=self.read, daemon=True)
         self.video = cv2.VideoCapture(video_path)
         self.height = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.width = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.nframes = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.queue = LifoQueue(maxsize=max_queue_size)
+        self.queue = deque(maxlen=max_queue_size)
         self.streaming = True
 
     def read(self):
         while self.streaming:
-            if not self.queue.full():
-                _, frame = self.video.read()
-                if frame is not None:
-                    self.queue.put(frame[:, :, ::-1])
-                else:
-                    self.stop()
+            _, frame = self.video.read()
+            if frame is not None:
+                self.queue.append(frame[:, :, ::-1])
+            else:
+                self.stop()
 
     def stop(self):
         self.streaming = False
@@ -41,60 +38,68 @@ class StreamReader(threading.Thread):
         print(f'fps={self.nframes / (end - start)}')
 
 
-class StreamDisplay(FigureCanvasTkAgg):
-    def __init__(self, stream, parent, dpi=100, **kwargs):
+class FastStreamDisplay(pg.GraphicsLayoutWidget):
+    def __init__(self, stream, parent=None):
+        super(FastStreamDisplay, self).__init__(parent)
+        # self.ci.layout.setContentsMargins(0, 0, 0, 0)
+        # self.ci.layout.setSpacing(0)
         self.stream = stream
         self.parent = parent
+        self.view = self.addViewBox()
+        self.view.setAspectLocked(True)
+        self.view.setRange(QtCore.QRectF(0, 0, self.stream.width, self.stream.height))
 
-        figsize = self.stream.width // dpi, self.stream.height // dpi
-        self.fig = plt.Figure(frameon=False, figsize=figsize, dpi=dpi)
-        super(StreamDisplay, self).__init__(self.fig, master=parent)
-        self.ax = self.fig.add_subplot(111)
-        img = np.random.random((self.stream.height, self.stream.width, 3))
-        self.im = self.ax.imshow(img, interpolation='none')
-        nbodyparts = 4
-        cmap = plt.cm.get_cmap('winter', nbodyparts)
-        colors = cmap(range(nbodyparts))
-        self.scat = self.ax.scatter([], [], **kwargs)
-        self.scat.set_color(colors)
-        self.ax.set_xlim(0, self.stream.width)
-        self.ax.set_ylim(0, self.stream.height)
-        self.ax.axis('off')
-        self.ax.invert_yaxis()
-        self.draw()
-        self.background = self.copy_from_bbox(self.ax.bbox)
+        self.img = pg.ImageItem()
+        self.points = pg.ScatterPlotItem()
+        self.view.addItem(self.img)
+        self.view.addItem(self.points)
 
-    def refresh(self, ms=10):
-        if not self.stream.queue.empty():
-            frame = self.stream.queue.get()
-            self.im.set_data(frame)
+        self.fps = 0.
+        self.last_update = time()
+
+    def refresh(self):
+        if self.stream.queue:
+            frame = self.stream.queue.pop()
+            self.img.setImage(np.rot90(frame, axes=(1, 0)))
             points = np.c_[np.random.randint(0, self.stream.height, 4),
                            np.random.randint(0, self.stream.width, 4)]
-            self.scat.set_offsets(points)
-            self.restore_region(self.background)
-            self.ax.draw_artist(self.im)
-            self.ax.draw_artist(self.scat)
-            self.blit(self.ax.bbox)
-        self.parent.after(ms, self.refresh)
+            self.points.setData(pos=points)
+
+        now = time()
+        dt = now - self.last_update
+        fps2 = 1.0 / max(dt, 0.000000000001)
+        self.last_update = now
+        self.fps = self.fps * 0.9 + fps2 * 0.1
+        tx = 'Mean Frame Rate:  {fps:.3f} FPS'.format(fps=self.fps)
+        self.parent.fps_label.setText(tx)
+        QtCore.QTimer.singleShot(1, self.refresh)
 
 
-class StreamWindow(tk.Toplevel):
-    def __init__(self, stream, parent, dpi=100, **kwargs):
-        super(StreamWindow, self).__init__(parent)
-        self.stream = stream
-        self.parent = parent
-        self.lift()
-        self.attributes('-topmost', True)
-        self.display = StreamDisplay(self.stream, self, dpi, **kwargs)
-        self.display.refresh()
-        self.start_button = tk.Button(self, text='Start', command=self.stream.start)
-        self.display.get_tk_widget().grid(row=0, column=0)
-        self.start_button.grid(row=1, column=0)
+class FastStreamWindow(QtWidgets.QMainWindow):
+    def __init__(self, stream, parent=None):
+        super(FastStreamWindow, self).__init__(parent)
+        self.setWindowTitle('DeepLabCut Live')
+        self.resize(stream.width, stream.height)
+
+        self.mainbox = QtWidgets.QWidget()
+        self.setCentralWidget(self.mainbox)
+        self.mainbox.setLayout(QtWidgets.QVBoxLayout())
+
+        self.fps_label = QtWidgets.QLabel()
+        self.canvas = FastStreamDisplay(stream, self)
+        self.mainbox.layout().addWidget(self.canvas)
+        self.mainbox.layout().addWidget(self.fps_label)
+
+        self.canvas.refresh()
+        self.canvas.stream.start()
 
 
 if __name__ == '__main__':
-    video = '/Users/Jessy/Documents/PycharmProjects/lactic/videos_hq/davidrudisha.mp4'
+    app = QtWidgets.QApplication(sys.argv)
+    # video = '/home/jessy/PycharmProjects/DLCdev/test-jessy-2020-01-28/videos/reachingvideo1.avi'
+    video = '/home/jessy/PycharmProjects/DLCdev/datasets/Horses-Byron-2019-05-08/videos/output4.mp4'
+    # video = '/home/jessy/PycharmProjects/DLCdev/MontBlanc-Daniel-2020-01-29/videos/montblanc.mov'
     stream = StreamReader(video)
-    root = tk.Tk()
-    window = StreamWindow(stream, root, s=12, alpha=0.7)
-    root.mainloop()
+    thisapp = FastStreamWindow(stream)
+    thisapp.show()
+    sys.exit(app.exec_())
